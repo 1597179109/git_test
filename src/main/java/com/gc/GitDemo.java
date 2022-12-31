6eb3fcfeba8ea649b0dbe2c04ee946ccc07072cb
Liyan <1597179109@qq.com>
2022-12-31 15:32:35 +0800

    这是第二次修改后的提交

--- src/main/java/com/gc/GitDemo.java
+++ src/main/java/com/gc/GitDemo.java
@@ -10,5 +10,6 @@ package com.gc;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
