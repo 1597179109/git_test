a23051f2287bbf6826f5e1b685ced6f5605fc956
Liyan <1597179109@qq.com>
2022-12-31 15:22:45 +0800

    这是第一次提交

---
+++ src/main/java/com/gc/GitDemo.java
@@ -0,0 +1,11 @@
+package com.gc;
+
+/**
+ * @projectName: git_test
+ * @packageName: com.gc
+ * @description:
+ * @author: Ghost_Code
+ * @date: 2022/12/31 13:30
+ */
+public class GitDemo {
+}
---
+++ src/main/java/com/gc/GitTest.java
@@ -0,0 +1,14 @@
+package com.gc;
+
+/**
+ * @projectName: git_test
+ * @packageName: com.gc
+ * @description:
+ * @author: Ghost_Code
+ * @date: 2022/12/31 13:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
